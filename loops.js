d5dc1757fb44d8ca6ed8bd7fc50a64b7ac53fca6
Daniel Killoran <dankilloran@gmail.com>
2019-04-01 22:45:38 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 var forLoop = function (abc) {
    for ( let i = 0; i < 25; i++) {
      if (i === 1) {
-       abc.unshift("I am 1 strange loop.")
+       abc.unshift('I am 1 strange loop.')
      } else {
-       abc.unshift("I am ${i} strange loops.")
+       abc.unshift('I am ${i} strange loops.')
      }
   }
   return abc;
