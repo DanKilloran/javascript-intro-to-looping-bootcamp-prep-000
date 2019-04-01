473c3126019c0db22fe7476ee75676636fc9f744
Daniel Killoran <dankilloran@gmail.com>
2019-04-01 23:01:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-function forLoop(abc) {
+function forLoop(array) {
    for ( let i = 0; i < 25; i++) {
      if (i === 1) {
-       abc.push('I am 1 strange loop.')
+       array.push(`I am 1 strange loop.`)
      } else {
-       abc.push('I am ${i} strange loops.')
+       array.push(`I am ${i} strange loops.`)
      }
   }
-  return abc;
+  return array;
 }
